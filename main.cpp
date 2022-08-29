7bf7da8d77e43a08488fa34e14525aa2e534b162
pushkar <pushkar.umrao@gmail.com>
2022-08-29 14:58:55 +0530

    factorial modified

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    cout<<"Enter a number ";
+    int n; cin>>n;
+    int fact=factorial(n);
+    if(fact>0) cout<<"Factorial of"<<n<<"is" << factorial(n)<< endl;
+    else cout<<"kindly enter positive number\n";
     return 0;
 }
