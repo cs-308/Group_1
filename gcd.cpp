838ae774a70af495c7beb3d0b9d16a7164bb6e9a
ExposedHost <arcade.ep@gmail.com>
2022-09-11 18:20:36 +0530

    added recursive gcd

--- gcd.cpp
+++ gcd.cpp
@@ -1,10 +1,7 @@
 
 int gcd(int n1,int n2){
-    while(n1 != n2) {
-        if(n1 > n2)
-        n1 -= n2;
-        else
-        n2 -= n1;
-    }
-    return n1;
+    if (n2 != 0)
+       return gcd(n2, n1 % n2);
+    else 
+       return n1;
 }
